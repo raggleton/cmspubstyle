8677f0b6ff20aef205c7b15818b1b6827553093b
Robin Aggleton <robin.aggleton@cern.ch>
2018-06-17 21:09:50 +0200

    Add more rules

--- rules/latex.py
+++ rules/latex.py
@@ -12,9 +12,10 @@ from rules.classes import *
 rules, tests = [], []
 
 rules.append(
-    Rule(description="Roman subscript in maths",
+    Rule(description="Use roman subscript in maths",
         # ignore if \mathrm or \text in subscript, but pickup other things
-         re_pattern=re.compile(r"_\{?(?!\\mathrm)(?!\\text)[\w\\]+\}?", re.IGNORECASE),
+         re_pattern=re.compile(r"^includegraphics[\w{}_\\=\[\].]*?_\{?(?!\\mathrm)(?!\\text)[\w\\]+\}?", re.IGNORECASE),
+         # re_pattern=re.compile(r"_\{?(?!\\mathrm)(?!\\text)[\w\\]+\}?", re.IGNORECASE),
          where=ALL())
 )
 tests.extend([
@@ -23,7 +24,7 @@ tests.extend([
     TestRule(rule=rules[-1], text="$p_{J2}$"),
     TestRule(rule=rules[-1], text="$p_{rec}$"),
     TestRule(rule=rules[-1], text="$p_{VLQ}$"),
-    TestRule(rule=rules[-1], text=r"$p_{\Zp}$"),  # user macro
+    TestRule(rule=rules[-1], text=r"$p_{\Zp}$", should_pass=True),  # user macro
     TestRule(rule=rules[-1], text=r"$p_{\mathrm{T}}$", should_pass=True),
     TestRule(rule=rules[-1], text=r"$p_{\mathrm{T}}$", should_pass=True),
     TestRule(rule=rules[-1], text=r"$p_{\mathrm{rec}}$", should_pass=True),
@@ -51,10 +52,38 @@ tests.extend([
 
 rules.append(
     Rule(description="Use \\etal macro",
-         re_pattern=re.compile(r"et al"),
+         re_pattern=re.compile(r" et al"),
          where=ALL())
 )
 tests.extend([
     TestRule(rule=rules[-1], text="Ram et al"),
     TestRule(rule=rules[-1], text="Dave \\etal", should_pass=True),
 ])
+
+# DASHES
+# rules.append(
+#     Rule(description="Use en dash -- for numerical range",
+#          # re_pattern=re.compile(r"\d+\s*?(-|---)\s*?\d+"),
+#          re_pattern=re.compile(r"(?<!cite)[\w{-]*?\d+\s*?(-|---)\s*?\d+"),
+#          where=ALL())
+# )
+# tests.extend([
+#     TestRule(rule=rules[-1], text="6-7"),
+#     TestRule(rule=rules[-1], text="6 - 7"),
+#     TestRule(rule=rules[-1], text="6---7"),
+#     TestRule(rule=rules[-1], text="6 -- 7", should_pass=True),
+#     TestRule(rule=rules[-1], text="6--7", should_pass=True),
+# ])
+
+# rules.append(
+#     Rule(description="Use en dash -- for two people",
+#          re_pattern=re.compile(r"^((?!cite).)*?[A-Z][a-z]*\s*?(-|---)\s*?[A-Z][a-z]*"),
+#          where=ALL())
+# )
+# tests.extend([
+#     TestRule(rule=rules[-1], text="Randall-Sundrum"),
+#     TestRule(rule=rules[-1], text="Randall - Sundrum"),
+#     TestRule(rule=rules[-1], text="Randall---Sundrum"),
+#     TestRule(rule=rules[-1], text="Randall -- Sundrum", should_pass=True),
+#     TestRule(rule=rules[-1], text="Randall--Sundrum", should_pass=True),
+# ])
--- rules/normal_text.py
+++ rules/normal_text.py
@@ -14,7 +14,7 @@ rules, tests = [], []
 ##############################################################################
 rules.append(
     Rule(description="Duplicate words",
-         re_pattern=re.compile(r"[\s.,](\w+)[\s.,]+\1[\s.,]+", re.IGNORECASE),
+         re_pattern=re.compile(r"\b(\w+)[\s.,]+\1[\s.,]+", re.IGNORECASE),
          where=ALL())
 )
 tests.extend([
@@ -27,16 +27,16 @@ tests.extend([
     TestRule(rule=rules[-1], text=" the. Then ", should_pass=True),
 ])
 
-rules.append(
-    Rule(description="Missing space at end of sentence",
-         re_pattern=re.compile(r"\.[A-Z]+"),
-         where=ALL())
-)
-tests.extend([
-    TestRule(rule=rules[-1], text="blah.Blah"),
-    TestRule(rule=rules[-1], text="v2.How"),
-    TestRule(rule=rules[-1], text="v2.3", should_pass=True),
-])
+# rules.append(
+#     Rule(description="Missing space at end of sentence",
+#          re_pattern=re.compile(r"\.[A-Z]+"),
+#          where=ALL())
+# )
+# tests.extend([
+#     TestRule(rule=rules[-1], text="blah.Blah"),
+#     TestRule(rule=rules[-1], text="v2.How"),
+#     TestRule(rule=rules[-1], text="v2.3", should_pass=True),
+# ])
 
 ##############################################################################
 # ABSTRACT ONLY
@@ -88,7 +88,7 @@ tests.extend([
 
 rules.append(
     Rule(description="No comma before et al",
-         re_pattern=re.compile(r", +\\etal"),
+         re_pattern=re.compile(r",\s+\\etal"),
          where=ALL())
 )
 tests.extend([
@@ -99,14 +99,17 @@ tests.extend([
 ##############################################################################
 # HYPHENATION
 ##############################################################################
+# UNHYPHENATED, 2 WORDS
 rules.append(
-    Rule(description="Missing hyphenation",
-         re_pattern=re.compile(r"\w tagged"),
+    Rule(description="Unnecessary hyphenation",
+         re_pattern=re.compile(r"\w-jet"),
          where=ALL())
 )
 tests.extend([
-    TestRule(rule=rules[-1], text="b tagged"),
-    TestRule(rule=rules[-1], text=r"Higgs-tagged", should_pass=True),
+    TestRule(rule=rules[-1], text="b-jet"),
+    TestRule(rule=rules[-1], text=r"\cPQb-jet"),
+    TestRule(rule=rules[-1], text=r"b jet", should_pass=True),
+    TestRule(rule=rules[-1], text=r"\cPQb jet", should_pass=True),
 ])
 
 rules.append(
@@ -133,4 +136,100 @@ tests.extend([
     TestRule(rule=rules[-1], text=r"b-tagged", should_pass=True),
     TestRule(rule=rules[-1], text=r"b-tagged", should_pass=True),
 ])
+
+rules.append(
+    Rule(description="Unnecessary hyphenation",
+         re_pattern=re.compile(r"\w-tagging"),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="b-tagging"),
+    TestRule(rule=rules[-1], text=r"b tagging", should_pass=True),
+    TestRule(rule=rules[-1], text=r"{\PW} tagging", should_pass=True),
+])
+
+# UNHYPHENATED SINGLE WORDS
+
+# SHOULD BE HYPHENATED
+rules.append(
+    Rule(description="Missing hyphenation",
+         re_pattern=re.compile(r" \w tagged", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text=" b tagged"),
+    TestRule(rule=rules[-1], text=r"Higgs-tagged", should_pass=True),
+    TestRule(rule=rules[-1], text="the tagged", should_pass=True),
+])
+
+
+##############################################################################
+# COMMON GRAMMATICAL MISTAKES
+##############################################################################
+rules.append(
+    Rule(description="Wrong indefinite article, requires an",
+         re_pattern=re.compile(r"a SM", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="a SM"),
+    TestRule(rule=rules[-1], text=r"an SM", should_pass=True),
+])
+
+rules.append(
+    Rule(description="Wrong indefinite article, requires a",
+         re_pattern=re.compile(r"\ban SUSY", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="an SUSY"),
+    TestRule(rule=rules[-1], text=r"a SUSY", should_pass=True),
+])
+
+rules.append(
+    Rule(description="'Due to' or 'Because of'?",
+         re_pattern=re.compile(r"\bdue to", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="due to"),
+    TestRule(rule=rules[-1], text=r"subdue to", should_pass=True),
+])
+
+rules.append(
+    Rule(description="'Evidence' takes no plural",
+         re_pattern=re.compile(r"\bevidences", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text=".Evidences"),
+    TestRule(rule=rules[-1], text="Evidences"),
+    TestRule(rule=rules[-1], text=r". Evidence for", should_pass=True),
+])
+
+rules.append(
+    Rule(description="'which' or 'that'?",
+         re_pattern=re.compile(r"(?<!in)(?<!,) +\bwhich", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="background which"),
+    TestRule(rule=rules[-1], text="in which", should_pass=True),
+    TestRule(rule=rules[-1], text=r"background, which", should_pass=True),
+])
+
+rules.append(
+    Rule(description="Do not use \"it's\"",
+         re_pattern=re.compile(r"\bit's", re.IGNORECASE),
+         where=ALL())
+)
+tests.extend([
+    TestRule(rule=rules[-1], text="it's"),
+    TestRule(rule=rules[-1], text="bit's", should_pass=True),
+    TestRule(rule=rules[-1], text=r"nits", should_pass=True),
+])
+
+##############################################################################
+# ACRONYMS
+##############################################################################
 
