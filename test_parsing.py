0c5aff36ec0d1e5ee05cccd224814024fea5dc1a
Robin Aggleton <robin.aggleton@cern.ch>
2018-06-17 20:28:49 +0200

    Update parsing tests to match new output from methods

--- test_parsing.py
+++ test_parsing.py
@@ -30,81 +30,94 @@ t = Text(doc.splitlines())
 def test_inline_maths():
     math_lines = t.iter_inline_delim("$")
     expect = [
-        TextLine(line_num=1, char_num_start=1, text=r"\PT > 50 \GeV"),
-        TextLine(line_num=5, char_num_start=1, text="12:30"),
+        Text([TextLine(line_num=1, char_num_start=1, text=r"\PT > 50 \GeV")]),
+        Text([TextLine(line_num=5, char_num_start=1, text="12:30")]),
     ]
     for e, m in zip(expect, math_lines):
-        assert(e.line_num == m.line_num)
-        assert(e.text == m.text)
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
+
+def test_inline_maths2():
     math_lines = t.iter_inline_delim(delim="$$")
     expect = [
-        TextLine(line_num=14,char_num_start=1, text="100")
+        Text([TextLine(line_num=14,char_num_start=1, text="100")])
     ]
     for e, m in zip(expect, math_lines):
-        assert(e.line_num == m.line_num)
-        assert(e.text == m.text)
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
 
 def test_environment_outer():
-    env_lines = list(t.iter_environment("chorus"))
-    expect = [[
-        TextLine(line_num=12, char_num_start=1, text=r"\begin{center}"),
-        TextLine(line_num=13, char_num_start=1, text=r"    It's gonna take a lot to take me away from you"),
-        TextLine(line_num=14, char_num_start=1, text=r"    There's nothing that a $100$ men or more could ever do"),
-        TextLine(line_num=15, char_num_start=1, text=r"    I bless the rains down in Africa"),
-        TextLine(line_num=16, char_num_start=1, text=r"    Gonna take some time to do the things we never had"),
-        TextLine(line_num=17, char_num_start=1, text=r"    \caption{Bah bah}\label{chorus1}"),
-        TextLine(line_num=18, char_num_start=1, text=r"\end{center}"),
-    ]]
-    for e, m in zip(expect[0], env_lines[0]):
-        assert(e.line_num == m.line_num)
-        assert(e.text == m.text)
+    env_lines = t.iter_environment("chorus")
+    expect = [
+        Text([
+            TextLine(line_num=12, char_num_start=1, text=r"\begin{center}"),
+            TextLine(line_num=13, char_num_start=1, text=r"    It's gonna take a lot to take me away from you"),
+            TextLine(line_num=14, char_num_start=1, text=r"    There's nothing that a $$100$$ men or more could ever do"),
+            TextLine(line_num=15, char_num_start=1, text=r"    I bless the rains down in Africa"),
+            TextLine(line_num=16, char_num_start=1, text=r"    Gonna take some time to do the things we never had"),
+            TextLine(line_num=17, char_num_start=1, text=r"    \caption{Bah bah}\label{chorus1}"),
+            TextLine(line_num=18, char_num_start=1, text=r"\end{center}"),
+        ])
+    ]
+    for e, m in zip(expect, env_lines):
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
 
 def test_environment_inner():
     env_lines = t.iter_environment("center")
     expect = [
-        [
+        Text([
             TextLine(line_num=13, char_num_start=1, text=r"    It's gonna take a lot to take me away from you"),
-            TextLine(line_num=14, char_num_start=1, text=r"    There's nothing that a $100$ men or more could ever do"),
+            TextLine(line_num=14, char_num_start=1, text=r"    There's nothing that a $$100$$ men or more could ever do"),
             TextLine(line_num=15, char_num_start=1, text=r"    I bless the rains down in Africa"),
             TextLine(line_num=16, char_num_start=1, text=r"    Gonna take some time to do the things we never had"),
             TextLine(line_num=17, char_num_start=1, text=r"    \caption{Bah bah}\label{chorus1}"),
-        ]
+        ])
     ]
     for e, m in zip(expect, env_lines):
-        for ee, mm in zip(e, m):
-            assert(ee.line_num == mm.line_num)
-            assert(ee.text == mm.text)
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
 
 def test_command():
     cmd_lines = t.iter_command("section")
     expect = [
-        [TextLine(line_num=1, char_num_start=1, text=r"Corrections for $\PT > 50 \GeV$")]
+        Text([TextLine(line_num=1, char_num_start=1, text=r"Corrections for $\PT > 50 \GeV$")])
     ]
     for e, m in zip(expect, cmd_lines):
-        for ee, mm in zip(e, m):
-            assert(ee.line_num == mm.line_num)
-            assert(ee.text == mm.text)
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
     cmd_lines = t.iter_command("drums")
     expect = [
-        [TextLine(line_num=10, char_num_start=1, text=r"yes")],
-        [TextLine(line_num=10, char_num_start=1, text=r"big")],
+        Text([TextLine(line_num=10, char_num_start=1, text=r"yes")]),
+        Text([TextLine(line_num=10, char_num_start=1, text=r"big")]),
     ]
     for e, m in zip(expect, cmd_lines):
-        for ee, mm in zip(e, m):
-            assert(ee.line_num == mm.line_num)
-            assert(ee.text == mm.text)
+        assert(len(e.text_contents) == len(m.text_contents))
+        for e_l, m_l in zip(e.text_contents, m.text_contents):
+            assert(e_l.line_num == m_l.line_num)
+            assert(e_l.text == m_l.text)
 
 def test_find():
     find_results = t.find_iter(re.compile(r"[\s.](\w+)[\s.]+\1", re.IGNORECASE))
     expect = [
-        [TextLine(line_num=7, char_num_start=1, text="I stopped an old man along the way way")]
+        TextLine(line_num=7, char_num_start=1, text="I stopped an old man along the way way")
     ]
     for e, m in zip(expect, find_results):
-        for ee, mm in zip(e, m[1]):
-            assert(ee.line_num == mm.line_num)
-            assert(ee.text == mm.text)
+        # find_iter returns (match, [TextLine]) hence the [1][0]
+        assert(e.line_num == m[1][0].line_num)
+        assert(e.text == m[1][0].text)
