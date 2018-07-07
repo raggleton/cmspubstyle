2e1ab7f99eb1c372e312e049c73f8eb0b6d37d3b
Robin Aggleton <robin.aggleton@cern.ch>
2018-07-07 22:41:32 +0100

    Update setup.py to use setuptools, add classifiers

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
 
-from distutils.core import setup
+import setuptools
 from cmspubstyle.__init__ import __version__
 
 
-setup(name='cmspubstyle',
-      version=__version__,
-      description='Check CMS publications against PubComm rules',
-      author='Robin Aggleton',
-      url='https://github.com/raggleton/CMS-grammar-checker',
-      packages=['cmspubstyle'],
-      scripts=['cmspubstyle/pubcheck.py'],
-     )
+setuptools.setup(name='cmspubstyle',
+                 version=__version__,
+                 description='Check CMS publications against PubComm rules',
+                 author='Robin Aggleton',
+                 url='https://github.com/raggleton/cmspubstyle',
+                 packages=setuptools.find_packages(),
+                 scripts=['cmspubstyle/pubcheck.py'],
+                 classifiers=(
+                     "License :: OSI Approved :: MIT License "
+                     "Programming Language :: Python :: 2.7",
+                     "Programming Language :: Python :: 3.6",
+                     "Topic :: Scientific/Engineering :: Physics",
+                     "Topic :: Text Processing"
+                 )
+                 )
